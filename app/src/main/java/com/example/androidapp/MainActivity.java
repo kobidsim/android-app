24e95a4b01d8fdcfafecf4e54ad09ffaab133e67
kobidsim <simkhadakobid@gmail.com>
2021-07-09 08:29:01 +0545

    added an icon, used fonts, used onclick event and intent to navigate on click of button

--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -2,13 +2,66 @@ package com.example.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button linearLayout;
+    private Button relativeLayout;
+    private Button frameLayout;
+    private Button tableLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        linearLayout = (Button) findViewById(R.id.navToLinearLayout);
+        relativeLayout = (Button) findViewById(R.id.navToRelativeLayout);
+        frameLayout = (Button) findViewById(R.id.navToFrameLayout);
+        tableLayout = (Button) findViewById(R.id.navToTableLayout);
+
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toLinear = new Intent(MainActivity.this, LinearLayout.class);
+                startActivity(toLinear);
+            }
+        });
+
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toRelative = new Intent(MainActivity.this, RelativeLayout.class);
+                startActivity(toRelative);
+            }
+        });
+
+        frameLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toFrame = new Intent(MainActivity.this, FrameLayout.class);
+                startActivity(toFrame);
+            }
+        });
+
+        tableLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toTable = new Intent(MainActivity.this, TableLayout.class);
+                startActivity(toTable);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/androidapp/RelativeLayout.java
+++ app/src/main/java/com/example/androidapp/RelativeLayout.java
@@ -2,13 +2,50 @@ package com.example.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class RelativeLayout extends AppCompatActivity {
+
+    private View parent;
+    private Button submit;
+    private TextView header;
+    private EditText input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relative_layout);
+
+        header = (TextView) findViewById(R.id.frontPageHeader);
+        input = (EditText) findViewById(R.id.nameInput);
+        submit = (Button) findViewById(R.id.submitButton1);
+        parent = (View) findViewById(R.id.relativeLayout);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Editable userInput = input.getText();
+                header.setText("Welcome back, "+userInput.toString());
+                //Toast.makeText(MainActivity.this, "You input: "+userInput, Toast.LENGTH_SHORT).show();
+                Snackbar.make(parent, "Your Input: "+userInput, Snackbar.LENGTH_LONG)
+                        .setAction("Again", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(RelativeLayout.this, "You input: "+userInput, Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setActionTextColor(Color.RED)
+                        .setTextColor(Color.YELLOW).show();
+            }
+        });
     }
 }
